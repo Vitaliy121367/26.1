3d6c5d34ff95698a99ce0e6230ac3a7b6ee12d29
Vitaliy <qw929496@gmail.com>
2024-09-19 17:46:36 +0300

    MyMath.cpp

---
+++ MyMath.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <cmath>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class MyMath {
+public:
+	static double PI();
+	static double add(double x, double y);
+	static double sub(double x, double y);
+	static double mult(double x, double y);
+	static double div(double x, double y);
+	static int random(int x, int y); 	//rand()%(y-x+1) + x;
+	static double abs(double x);  //std::abs()
+	static double round(double x);
+	static double floor(double x);
+	static double max(double x, double y);
+	static double min(double x, double y);
+	static double pow(double x, int y);
+};
