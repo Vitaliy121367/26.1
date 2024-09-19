326cd687060ae03b3d7501927f4c0ba328d006c8
Vitaliy <qw929496@gmail.com>
2024-09-19 17:48:18 +0300

    main

--- 26.1.cpp
+++ 26.1.cpp
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <cstdio>
 #include <cstring>
+#include "MyMath.h"
 using namespace std;
 
 int main()
@@ -13,6 +14,18 @@ int main()
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
+    cout << "PI: " << MyMath::PI() << endl;
+    cout << "+: " << MyMath::add(1, 1) << endl;
+    cout << "-: " << MyMath::sub(4, 1) << endl;
+    cout << "*: " << MyMath::mult(4, 2) << endl;
+    cout << "/: " << MyMath::div(4, 2) << endl;
+    cout << "Rand: " << MyMath::random(1, 10) << endl;
+    cout << "Abs: " << MyMath::abs(-17) << endl;
+    cout << "ceil: " << MyMath::round(17.9) << endl;
+    cout << "floor: " << MyMath::floor(17.9) << endl;
+    cout << "min: " << MyMath::min(4, 2) << endl;
+    cout << "max: " << MyMath::max(4, 2) << endl;
+    cout << "pow: " << MyMath::pow(2, 3) << endl;
 
 
     return 0;
